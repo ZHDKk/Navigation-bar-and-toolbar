3eb5b23df0daa1066ba5dd6cdbf6a2b48aa2e118
ZHDKk <1083861095@qq.com>
2017-11-08 10:17:18 +0800

    第一次提交

---
+++ 导航栏和工具栏/VCSecond.m
@@ -0,0 +1,38 @@
+//
+//  VCSecond.m
+//  导航栏和工具栏
+//
+//  Created by zh dk on 2017/8/25.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import "VCSecond.h"
+
+@interface VCSecond ()
+
+@end
+
+@implementation VCSecond
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor=[UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ 导航栏和工具栏/VCroot.h
@@ -0,0 +1,13 @@
+//
+//  VCroot.h
+//  导航栏和工具栏
+//
+//  Created by zh dk on 2017/8/25.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCroot : UIViewController
+
+@end
